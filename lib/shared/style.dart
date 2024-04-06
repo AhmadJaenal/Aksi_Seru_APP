977f96001079881e6303d96720a7a930c4b0938a
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-07 05:54:01 +0700

    Setup Style

--- lib/shared/style.dart
+++ lib/shared/style.dart
@@ -2,79 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppColors {
-  static Color primary1 = const Color(0xFFE74C3C);
-  static Color primary2 = const Color(0xFFF38276);
-  static Color primary3 = const Color(0xFFF38276);
-  static Color primary4 = const Color(0xFFFDE5E3);
+  static Color primary1 = const Color(0xFF2F7632);
+  static Color primary10 = const Color(0xFFD7E8D8);
 
-  static Color secondary1 = const Color(0xFF9B59B6);
-  static Color secondary2 = const Color(0xFFE26BF5);
-  static Color secondary3 = const Color(0xFFF7C2FF);
-  static Color secondary4 = const Color(0xFFFDEFFC);
+  static Color secondary = const Color(0xFFFFFFFF);
 
-  static Color warning1 = const Color(0xFFFFC212);
-  static Color warning2 = const Color(0xFFFFD252);
-  static Color warning3 = const Color(0xFFFFE393);
-  static Color warning4 = const Color(0xFFFFF9E9);
-
-  static Color success1 = const Color(0xFF00F7BF);
-  static Color success2 = const Color(0xFF5BFFCE);
-  static Color success3 = const Color(0xFF98FFE0);
-  static Color success4 = const Color(0xFFEAFFF8);
-
-  static Color danger1 = const Color(0xFFFF0022);
-  static Color danger2 = const Color(0xFFFF455E);
-
-  static Color info1 = const Color(0xFF3498DB);
-  static Color info2 = const Color(0xFF5BFFCE);
-  static Color info3 = const Color(0xFF97F4FF);
-  static Color info4 = const Color(0xFFEAFCFF);
-
-  static Color black = const Color(0xFF111314);
-  static Color black100 = const Color(0xFF2D3436);
-  static Color black80 = const Color(0xFF636E72);
-  static Color black60 = const Color(0xFFB2BEC3);
-  static Color black40 = const Color(0xFFDFE6E9);
-  static Color black20 = const Color(0xFFDFE6E9);
-  static Color black10 = const Color(0xFFF7FAFC);
-
-  static Color white = const Color(0xFFFFFFFF);
-  static Color blue = const Color(0xFF0B63E5);
-  static Color green = const Color(0xFFA3D139);
-  static Color darkBlue = const Color(0xFF273B4A);
+  static Color greyColor = const Color(0xFF667085);
+  static Color whiteColor = const Color(0xFFFFFFFF);
+  static Color blackColor = const Color(0xFF1D2939);
+  static Color redColor = const Color(0xFFD92D20);
+  static Color roseColor = const Color(0xFFFFE4E8);
 }
 
 class AppTextStyle {
-  static TextStyle title = GoogleFonts.manrope()
-      .copyWith(fontSize: 48, fontWeight: AppWeightFont.extraBold);
+  static TextStyle titlePrimary = GoogleFonts.playfair().copyWith(
+      fontSize: 48,
+      fontWeight: AppWeightFont.medium,
+      color: AppColors.primary1);
 
-  static TextStyle h1 = GoogleFonts.manrope()
+  static TextStyle h1 = GoogleFonts.playfair()
       .copyWith(fontSize: 32, fontWeight: AppWeightFont.bold);
-  static TextStyle h2 = GoogleFonts.manrope()
+  static TextStyle h2 = GoogleFonts.playfair()
       .copyWith(fontSize: 24, fontWeight: AppWeightFont.bold);
-  static TextStyle h3 = GoogleFonts.manrope()
+  static TextStyle h3 = GoogleFonts.inter()
       .copyWith(fontSize: 20, fontWeight: AppWeightFont.bold);
 
-  static TextStyle paragraphL = GoogleFonts.manrope()
+  static TextStyle paragraphL = GoogleFonts.inter()
       .copyWith(fontSize: 16, fontWeight: AppWeightFont.reguler);
-  static TextStyle paragraphM = GoogleFonts.manrope()
-      .copyWith(fontSize: 14, fontWeight: AppWeightFont.reguler);
-  static TextStyle paragraphS = GoogleFonts.manrope()
-      .copyWith(fontSize: 12, fontWeight: AppWeightFont.reguler);
-  static TextStyle paragraphXS = GoogleFonts.manrope()
-      .copyWith(fontSize: 10, fontWeight: AppWeightFont.reguler);
 
-  static TextStyle paragraphLBold = GoogleFonts.manrope()
-      .copyWith(fontSize: 16, fontWeight: AppWeightFont.bold);
-  static TextStyle paragraphMBold = GoogleFonts.manrope()
-      .copyWith(fontSize: 14, fontWeight: AppWeightFont.bold);
-  static TextStyle paragraphSBold = GoogleFonts.manrope()
-      .copyWith(fontSize: 12, fontWeight: AppWeightFont.bold);
-  static TextStyle paragraphXSBold = GoogleFonts.manrope()
-      .copyWith(fontSize: 10, fontWeight: AppWeightFont.bold);
-
-  static TextStyle paragraphSecondaryS = GoogleFonts.lexend()
-      .copyWith(fontSize: 14, fontWeight: AppWeightFont.light);
+  static TextStyle appbarTitle = GoogleFonts.playfair().copyWith(
+      fontSize: 36, fontWeight: AppWeightFont.bold, color: AppColors.primary1);
 }
 
 class AppWeightFont {
@@ -87,5 +44,5 @@ class AppWeightFont {
 }
 
 class AppMargin {
-  static double defaultMargin = 20;
+  static double defaultMargin = 24;
 }
