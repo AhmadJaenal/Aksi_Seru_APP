2ab0275ee0af0ba06c1b7e02994917410059c70a
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-07 14:37:04 +0700

    Update UI

--- lib/screens/home/recommendation_page.dart
+++ lib/screens/home/recommendation_page.dart
@@ -1,8 +1,7 @@
 import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/card_article.dart';
 import 'package:aksi_seru_app/widgets/user_profile.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 class RecommendationPage extends StatelessWidget {
@@ -14,7 +13,7 @@ class RecommendationPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(378),
+          preferredSize: const Size.fromHeight(390),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -97,98 +96,15 @@ class RecommendationPage extends StatelessWidget {
                   ),
                 ),
               ),
+              const Gap(8),
             ],
           ),
         ),
         body: ListView.builder(
+          physics: const BouncingScrollPhysics(),
           itemCount: 10,
           itemBuilder: (context, index) {
-            return Container(
-              margin: const EdgeInsets.symmetric(vertical: 5),
-              padding:
-                  EdgeInsets.symmetric(horizontal: AppMargin.defaultMargin),
-              width: double.infinity,
-              child: Row(
-                children: [
-                  Image.asset('assets/article.png', height: width * .4),
-                  const Gap(12),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset('assets/user_profile.png', width: 24),
-                          const Gap(4),
-                          Text(
-                            'Alexander K..',
-                            style: AppTextStyle.paragraphL.copyWith(
-                              color: AppColors.blackColor,
-                            ),
-                          ),
-                          const Gap(7),
-                          Container(
-                            width: 18,
-                            height: 18,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: AppColors.primary1,
-                            ),
-                            child: Icon(
-                              Icons.check,
-                              color: AppColors.whiteColor,
-                              size: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Gap(8),
-                      Container(
-                        width: 160,
-                        height: 62,
-                        child: Text(
-                          'Edukasi Konservasi Alam dan Lingkungan Hidup',
-                          maxLines: 4,
-                          overflow: TextOverflow.ellipsis,
-                          style: AppTextStyle.paragraphL.copyWith(
-                            color: AppColors.blackColor,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        '6 mnt baca Okt 23, 2023',
-                        style: AppTextStyle.paragraphM.copyWith(
-                          color: AppColors.greyColor,
-                        ),
-                      ),
-                      const Gap(5),
-                      Row(
-                        children: [
-                          Container(
-                            height: 26,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 2),
-                            decoration: BoxDecoration(
-                              color: AppColors.greyColor.withOpacity(.3),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Text(
-                              'Edukasi',
-                              style: AppTextStyle.paragraphL.copyWith(
-                                color: AppColors.greyColor,
-                              ),
-                            ),
-                          ),
-                          const Gap(50),
-                          Image.asset('assets/icon_boormark.png', width: 24),
-                          Image.asset('assets/icon_option.png', width: 24),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
+            return const CardArticle();
           },
         ),
       ),
