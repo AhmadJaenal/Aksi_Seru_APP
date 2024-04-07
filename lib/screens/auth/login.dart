29dbfa5929e32e58e54bae7ba47f9b1e0cb18d8e
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-07 08:40:27 +0700

    [+] Add login page

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,66 @@
+import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/custom_button.dart';
+import 'package:aksi_seru_app/widgets/custom_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: AppMargin.defaultMargin),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(160),
+            Text(
+              'Login',
+              style: AppTextStyle.titlePrimary,
+            ),
+            const Gap(24),
+            CustomTextField(
+              hintText: 'Masukan email',
+              icon: Icon(
+                Icons.email_outlined,
+                color: AppColors.greyColor,
+              ),
+            ),
+            const Gap(12),
+            const CustomTextFieldPassword(hintText: 'Masukan kata sandi'),
+            const Gap(24),
+            PrimaryButton(ontap: () {}, title: 'Lanjut'),
+            const Spacer(),
+            Center(
+              child: Text(
+                'Belum punya akun?',
+                style: AppTextStyle.paragraphL.copyWith(
+                  color: AppColors.greyColor,
+                ),
+              ),
+            ),
+            const Gap(14),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Daftar ',
+                  style: AppTextStyle.paragraphL.copyWith(
+                    color: AppColors.primary1,
+                  ),
+                ),
+                const Gap(8),
+                Icon(Icons.arrow_forward_rounded,
+                    color: AppColors.primary1, size: 24)
+              ],
+            ),
+            const Gap(40),
+          ],
+        ),
+      ),
+    );
+  }
+}
