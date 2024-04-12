f636772cc54bc602c7d7a38b1f535cb9e65d9e62
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-12 08:59:13 +0700

    [+] Add post model

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,1 +1,25 @@
+class PostModel {
+  final int idPost;
+  final String url;
+  final int countLike;
+  final int countComment;
+  final String date;
 
+  PostModel({
+    required this.idPost,
+    required this.url,
+    required this.countLike,
+    required this.countComment,
+    required this.date,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      idPost: json['idPost'],
+      url: json['url'],
+      countLike: json['countLike'],
+      countComment: json['countComment'],
+      date: json['date'],
+    );
+  }
+}
