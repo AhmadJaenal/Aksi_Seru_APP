d2298b7841925b6e845415f7bc9a69174c93bd40
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-12 10:39:06 +0700

    [+] Add page menu article

--- lib/screens/home/article/trending_page.dart
+++ lib/screens/home/article/trending_page.dart
@@ -1,10 +1,30 @@
+import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/card_article.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class TrendingPage extends StatelessWidget {
   const TrendingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Gap(AppMargin.defaultMargin),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => const CardArticle(),
+              childCount: 5,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Gap(AppMargin.defaultMargin),
+          ),
+        ],
+      ),
+    );
   }
 }
