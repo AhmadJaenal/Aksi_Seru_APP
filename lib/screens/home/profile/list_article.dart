1548825541d07dd1e3770791168ff3be78052b33
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-12 19:35:50 +0700

    [+] Add new page

---
+++ lib/screens/home/profile/list_article.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListArticle extends StatelessWidget {
+  const ListArticle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/home/profile/list_post.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListPost extends StatelessWidget {
+  const ListPost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
