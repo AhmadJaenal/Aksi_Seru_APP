82a8fbbd9735df0e8088a1c2bc35a0d101ecd789
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-14 08:37:49 +0700

    [+] Add widget article

--- lib/screens/home/profile/list_article.dart
+++ lib/screens/home/profile/list_article.dart
@@ -1,10 +1,39 @@
+import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/card_article.dart';
+import 'package:aksi_seru_app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 
 class ListArticle extends StatelessWidget {
   const ListArticle({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.all(AppMargin.defaultMargin),
+              child: MiniButton(
+                icon: 'icon_pen.png',
+                ontap: () {},
+                title: 'Tulis artikel',
+                color: AppColors.primary1,
+                titleColor: AppColors.primary1,
+                iconColor: AppColors.primary1,
+              ),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            childCount: 10,
+            (context, index) => const CardArticle(),
+          ))
+        ],
+      ),
+    );
   }
 }
