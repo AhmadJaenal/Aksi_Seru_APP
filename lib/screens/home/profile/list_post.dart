ae989b074d71e824fd977de4434e37e9affba6e6
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-14 08:30:28 +0700

    [+] Add CardPost

--- lib/screens/home/profile/list_post.dart
+++ lib/screens/home/profile/list_post.dart
@@ -1,10 +1,26 @@
+import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/card_post.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ListPost extends StatelessWidget {
   const ListPost({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: Gap(AppMargin.defaultMargin)),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+            childCount: 10,
+            (context, index) => const CardPost(),
+          ))
+        ],
+      ),
+    );
   }
 }
