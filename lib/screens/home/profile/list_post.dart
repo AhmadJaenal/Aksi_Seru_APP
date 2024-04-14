2afb9742753aa5845db8563d58f58d0483d357c6
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-14 08:32:24 +0700

    [^] Update widget

--- lib/screens/home/profile/list_post.dart
+++ lib/screens/home/profile/list_post.dart
@@ -1,7 +1,9 @@
 import 'package:aksi_seru_app/shared/style.dart';
 import 'package:aksi_seru_app/widgets/card_post.dart';
+import 'package:aksi_seru_app/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 
 class ListPost extends StatelessWidget {
   const ListPost({super.key});
@@ -13,7 +15,45 @@ class ListPost extends StatelessWidget {
       backgroundColor: AppColors.whiteColor,
       body: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(child: Gap(AppMargin.defaultMargin)),
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.all(AppMargin.defaultMargin),
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+                border: Border(
+                  bottom: BorderSide(
+                    color: AppColors.greyColor.withOpacity(.2),
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: width * .7,
+                    child: const CustomTextField(
+                        hintText: 'Share hal seru kalian hari ini, yuk!'),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      backgroundColor:
+                          MaterialStatePropertyAll(AppColors.primary1),
+                      padding:
+                          const MaterialStatePropertyAll(EdgeInsets.all(12)),
+                    ),
+                    onPressed: () => Get.toNamed('/create-post'),
+                    icon: Image.asset('assets/icon_image.png', width: 24),
+                  )
+                ],
+              ),
+            ),
+          ),
           SliverList(
               delegate: SliverChildBuilderDelegate(
             childCount: 10,
