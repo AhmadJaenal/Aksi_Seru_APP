cc0fc3f76db43efff979bd7616e2e2cde575bce4
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-04-18 10:57:51 +0700

    Set token auth

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ import 'package:aksi_seru_app/screens/home/search/search_page.dart';
 import 'package:aksi_seru_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dart:developer' as developer;
 
 void main() {
   runApp(const MyApp());
@@ -37,105 +40,130 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  Future<void> setToken() async {
+    final SharedPreferences prefs = await _prefs;
+    await prefs.setString('token', '');
+  }
+
+  Future<Widget> getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    if (token!.isEmpty) {
+      return const CheckConnection(page: SplashScreen());
+    } else {
+      return CheckConnection(page: FeedPage());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const CheckConnection(page: SplashScreen()),
-      getPages: [
-        GetPage(
-          name: '/splash-screen',
-          page: () => const SplashScreen(),
-        ),
-        GetPage(
-          name: '/login',
-          page: () => CheckConnection(page: Login()),
-        ),
-        GetPage(
-          name: '/register',
-          page: () => CheckConnection(page: Register()),
-        ),
-        GetPage(
-          name: '/verified-code',
-          page: () => CheckConnection(page: VerifiedCode()),
-        ),
-        GetPage(
-          name: '/create-password',
-          page: () => CheckConnection(page: CreatePassword()),
-        ),
-        GetPage(
-          name: '/success-register',
-          page: () => const CheckConnection(page: SuccessRegister()),
-        ),
-        GetPage(
-          name: '/create-username',
-          page: () => CheckConnection(page: CreateUsername()),
-        ),
-        GetPage(
-          name: '/recommendation-page',
-          page: () => const CheckConnection(page: RecommendationPage()),
-        ),
-        GetPage(
-          name: '/feed-page',
-          page: () => CheckConnection(page: FeedPage()),
-        ),
-        GetPage(
-          name: '/create-post',
-          page: () => CheckConnection(page: CreatePost()),
-        ),
-        GetPage(
-          name: '/story',
-          page: () => CheckConnection(page: StoryView()),
-        ),
-        GetPage(
-          name: '/review-post',
-          page: () => const CheckConnection(page: ReviewPost()),
-        ),
-        GetPage(
-          name: '/notification-page',
-          page: () => const CheckConnection(page: NotificationPage()),
-        ),
-        GetPage(
-          name: '/nav-bar',
-          page: () => const CustomNavBottom(),
-        ),
-        GetPage(
-          name: '/search-page',
-          page: () => CheckConnection(page: SearchPage()),
-        ),
-        GetPage(
-          name: '/article-page',
-          page: () => CheckConnection(page: ArticlePage()),
-        ),
-        GetPage(
-          name: '/detail-article',
-          page: () => const CheckConnection(page: DetailArticle()),
-        ),
-        GetPage(
-          name: '/trending-article',
-          page: () => const CheckConnection(page: TrendingPage()),
-        ),
-        GetPage(
-          name: '/for-you-article',
-          page: () => const CheckConnection(page: ForYouPage()),
-        ),
-        GetPage(
-          name: '/following-article',
-          page: () => const CheckConnection(page: FollowingPage()),
-        ),
-        GetPage(
-          name: '/create-article',
-          page: () => const CheckConnection(page: CreateArticle()),
-        ),
-        GetPage(
-          name: '/verified-profile',
-          page: () => const CheckConnection(page: VerifiedProfile()),
-        ),
-        GetPage(
-          name: '/message',
-          page: () => CheckConnection(page: Message()),
-        ),
-      ],
-    );
+    return FutureBuilder<Widget>(
+        future: getToken(),
+        builder: (context, snapshot) {
+          developer.log(snapshot.data.toString(), name: 'data snapshot');
+          if (snapshot.hasData) {
+            return GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: snapshot.data,
+              getPages: [
+                GetPage(
+                  name: '/splash-screen',
+                  page: () => const SplashScreen(),
+                ),
+                GetPage(
+                  name: '/login',
+                  page: () => CheckConnection(page: Login()),
+                ),
+                GetPage(
+                  name: '/register',
+                  page: () => CheckConnection(page: Register()),
+                ),
+                GetPage(
+                  name: '/verified-code',
+                  page: () => CheckConnection(page: VerifiedCode()),
+                ),
+                GetPage(
+                  name: '/create-password',
+                  page: () => CheckConnection(page: CreatePassword()),
+                ),
+                GetPage(
+                  name: '/success-register',
+                  page: () => const CheckConnection(page: SuccessRegister()),
+                ),
+                GetPage(
+                  name: '/create-username',
+                  page: () => CheckConnection(page: CreateUsername()),
+                ),
+                GetPage(
+                  name: '/recommendation-page',
+                  page: () => const CheckConnection(page: RecommendationPage()),
+                ),
+                GetPage(
+                  name: '/feed-page',
+                  page: () => CheckConnection(page: FeedPage()),
+                ),
+                GetPage(
+                  name: '/create-post',
+                  page: () => CheckConnection(page: CreatePost()),
+                ),
+                GetPage(
+                  name: '/story',
+                  page: () => CheckConnection(page: StoryView()),
+                ),
+                GetPage(
+                  name: '/review-post',
+                  page: () => const CheckConnection(page: ReviewPost()),
+                ),
+                GetPage(
+                  name: '/notification-page',
+                  page: () => const CheckConnection(page: NotificationPage()),
+                ),
+                GetPage(
+                  name: '/nav-bar',
+                  page: () => const CustomNavBottom(),
+                ),
+                GetPage(
+                  name: '/search-page',
+                  page: () => CheckConnection(page: SearchPage()),
+                ),
+                GetPage(
+                  name: '/article-page',
+                  page: () => CheckConnection(page: ArticlePage()),
+                ),
+                GetPage(
+                  name: '/detail-article',
+                  page: () => const CheckConnection(page: DetailArticle()),
+                ),
+                GetPage(
+                  name: '/trending-article',
+                  page: () => const CheckConnection(page: TrendingPage()),
+                ),
+                GetPage(
+                  name: '/for-you-article',
+                  page: () => const CheckConnection(page: ForYouPage()),
+                ),
+                GetPage(
+                  name: '/following-article',
+                  page: () => const CheckConnection(page: FollowingPage()),
+                ),
+                GetPage(
+                  name: '/create-article',
+                  page: () => const CheckConnection(page: CreateArticle()),
+                ),
+                GetPage(
+                  name: '/verified-profile',
+                  page: () => const CheckConnection(page: VerifiedProfile()),
+                ),
+                GetPage(
+                  name: '/message',
+                  page: () => CheckConnection(page: Message()),
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
