0eb86ae13a1d41a73634407c987acbff1aade751
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-05-04 20:10:21 +0700

    [+] Add new page

---
+++ lib/screens/home/profile/list_following.dart
@@ -0,0 +1,94 @@
+import 'package:aksi_seru_app/controller/user.dart';
+import 'package:aksi_seru_app/shared/style.dart';
+import 'package:aksi_seru_app/widgets/custom_button.dart';
+import 'package:aksi_seru_app/widgets/user_profile.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class ListFollowing extends StatelessWidget {
+  const ListFollowing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    UserData.listFollowing();
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(96),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: AppColors.greyColor.withOpacity(.2),
+                  width: 1,
+                ),
+              ),
+            ),
+            padding: EdgeInsets.all(AppMargin.defaultMargin),
+            child: Row(
+              children: [
+                const BackButtonCustom(),
+                const Gap(16),
+                Text(
+                  'List Following',
+                  style: AppTextStyle.appbarTitle.copyWith(
+                    color: AppColors.primary1,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: AppMargin.defaultMargin, vertical: 5),
+              child: SizedBox(
+                width: 128,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/user_profile.png', width: 60),
+                    const Gap(8),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Jenal',
+                              style: AppTextStyle.paragraphL.copyWith(
+                                color: AppColors.blackColor,
+                              ),
+                            ),
+                            const Gap(7),
+                            const Verified(),
+                          ],
+                        ),
+                        Text(
+                          'Bio',
+                          style: AppTextStyle.paragraphL.copyWith(
+                            color: AppColors.blackColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    const FollowButton(
+                      idUser: 1,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
