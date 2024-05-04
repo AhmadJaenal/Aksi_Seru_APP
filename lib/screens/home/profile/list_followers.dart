a45b6b45130f855b4a0d60a49310aed30f4fccb5
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-05-04 20:37:25 +0700

    Fetch data followers

--- lib/screens/home/profile/list_followers.dart
+++ lib/screens/home/profile/list_followers.dart
@@ -1,16 +1,18 @@
 import 'package:aksi_seru_app/controller/user.dart';
+import 'package:aksi_seru_app/models/user_model.dart';
 import 'package:aksi_seru_app/shared/style.dart';
 import 'package:aksi_seru_app/widgets/custom_button.dart';
 import 'package:aksi_seru_app/widgets/user_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import 'dart:developer' as developer;
+
 class ListFollowers extends StatelessWidget {
   const ListFollowers({super.key});
 
   @override
   Widget build(BuildContext context) {
-    UserData.listFollowing();
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
@@ -30,7 +32,7 @@ class ListFollowers extends StatelessWidget {
                 const BackButtonCustom(),
                 const Gap(16),
                 Text(
-                  'List Followers',
+                  'Followers',
                   style: AppTextStyle.appbarTitle.copyWith(
                     color: AppColors.primary1,
                   ),
@@ -40,52 +42,66 @@ class ListFollowers extends StatelessWidget {
             ),
           ),
         ),
-        body: ListView.builder(
-          physics: const BouncingScrollPhysics(),
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: AppMargin.defaultMargin, vertical: 5),
-              child: SizedBox(
-                width: 128,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/user_profile.png', width: 60),
-                    const Gap(8),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+        body: FutureBuilder(
+          future: UserData.listFollowers(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    UserModel userData = snapshot.data![index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: AppMargin.defaultMargin, vertical: 5),
+                      child: SizedBox(
+                        width: 128,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Text(
-                              'Jenal',
-                              style: AppTextStyle.paragraphL.copyWith(
-                                color: AppColors.blackColor,
-                              ),
+                            Image.asset('assets/user_profile.png', width: 60),
+                            const Gap(8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      userData.name,
+                                      style: AppTextStyle.paragraphL.copyWith(
+                                        color: AppColors.blackColor,
+                                      ),
+                                    ),
+                                    const Gap(7),
+                                    const Verified(),
+                                  ],
+                                ),
+                                Text(
+                                  userData.bio,
+                                  style: AppTextStyle.paragraphL.copyWith(
+                                    color: AppColors.blackColor,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const Gap(7),
-                            const Verified(),
+                            const Spacer(),
+                            FollowButton(
+                              idUser: userData.id,
+                            ),
                           ],
                         ),
-                        Text(
-                          'Bio',
-                          style: AppTextStyle.paragraphL.copyWith(
-                            color: AppColors.blackColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Spacer(),
-                    const FollowButton(
-                      idUser: 1,
-                    ),
-                  ],
-                ),
-              ),
-            );
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
           },
         ),
       ),
