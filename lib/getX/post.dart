7aeeb92d4aeea1fd30d6e3db7b5ca8c828dbfcf2
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-05-17 20:24:03 +0700

    [-] Delete unused code

--- lib/getX/post.dart
+++ lib/getX/post.dart
@@ -1,4 +1,5 @@
 import 'package:aksi_seru_app/controller/post_controller.dart';
+import 'package:aksi_seru_app/models/post_model.dart';
 import 'package:get/get.dart';
 import 'dart:developer' as developer;
 
@@ -9,15 +10,16 @@ class LikeUnlikePost extends GetxController {
 
   void setLikeUnlike({int? id}) {
     if (likeState.value) {
-      developer.log('like');
-      PostController.likePost(id: id);
-    } else {
       developer.log('unlike');
       PostController.unLikePost(id: id);
+    } else {
+      developer.log('like');
+      PostController.likePost(id: id);
     }
     likeState.value = !likeState.value;
   }
 }
+
 class CommentState extends GetxController {
   var comment = <CommentModel>[].obs;
 
