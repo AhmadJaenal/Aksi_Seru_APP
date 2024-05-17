b1d0c98afb52db95e9026bba155590dc36c99ba5
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-05-17 20:24:30 +0700

    [-] Delete artibut

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -46,24 +46,29 @@ class LikeModel {
       postId: json['post_id'],
     );
   }
+
   static List<LikeModel> fromJsonList(List<dynamic> jsonList) {
     return jsonList.map((json) => LikeModel.fromJson(json)).toList();
   }
 }
+
 class CommentModel {
   final int id;
+  final int userId;
   final int postId;
-  final int comment;
+  final String comment;
 
   CommentModel({
     required this.id,
+    required this.userId,
     required this.postId,
     required this.comment,
   });
   factory CommentModel.fromJson(Map<String, dynamic> json) {
     return CommentModel(
       id: json['id'],
-      postId: json['post_id'],
+      userId: json['user_id'] ?? 0,
+      postId: json['post_id'] ?? 0,
       comment: json['comment'],
     );
   }
