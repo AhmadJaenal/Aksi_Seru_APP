7b2dd52e5142a094d2ebcfb91ca9a4ce52a4be99
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-01 17:14:05 +0700

    [^] Update model article

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,25 +1,41 @@
 class ArticleModel {
-  final int idArticle;
-  final String idUser;
-  final List<String> tag;
+  final int id;
+  final int userId;
+  final String title;
+  final String subtitle;
+  final String content;
+  final int countlike;
   final int countComment;
-  final String date;
+  final String urlImage;
+  final String updatedAt;
 
   ArticleModel({
-    required this.idArticle,
-    required this.idUser,
-    required this.tag,
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.subtitle,
+    required this.content,
+    required this.countlike,
     required this.countComment,
-    required this.date,
+    required this.urlImage,
+    required this.updatedAt,
   });
 
   factory ArticleModel.fromJson(Map<String, dynamic> json) {
     return ArticleModel(
-      idArticle: json['idArticle'],
-      idUser: json['idUser'],
-      tag: json['tag'],
-      countComment: json['countComment'],
-      date: json['date'],
+      id: json['id'],
+      userId: json['user_id'],
+      title: json['subtitle'],
+      subtitle: json['title'],
+      content: json['content'],
+      countlike: json['countlike'],
+      countComment: json['countcomment'],
+      urlImage: json['urlimage'],
+      updatedAt: json['updated_at'],
     );
+  }
+
+  static List<ArticleModel> fromJsonList(List<dynamic> jsonList) {
+    return jsonList.map((json) => ArticleModel.fromJson(json)).toList();
   }
 }
