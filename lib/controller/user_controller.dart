88fa14951fab3e505a6ae934d288cd2e355829ad
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-16 11:06:36 +0700

    [+] Update profile user

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:aksi_seru_app/controller/post_controller.dart';
 import 'package:aksi_seru_app/models/user_model.dart';
+import 'package:aksi_seru_app/widgets/custom_popup.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:developer' as developer;
@@ -35,6 +40,61 @@ class UserData extends GetxController {
       developer.log(e.toString(), name: 'catch error');
       return null;
     });
+  }
+
+  static updateProfile({String? name, bio, File? image}) async {
+    Map<String, dynamic> updatedData = {
+      "full_name": name,
+      "bio": bio,
+    };
+
+    PostController postController = PostController();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? email = prefs.getString("email");
+
+    try {
+      if (image != null) {
+        String imageUrl = await PostController.uploadImage(image);
+        updatedData["avatar"] = imageUrl;
+
+        QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+            .collection("users")
+            .where("email", isEqualTo: email)
+            .get();
+
+        if (querySnapshot.docs.isNotEmpty) {
+          if (querySnapshot.docs.isNotEmpty) {
+            for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+              await doc.reference.update(updatedData);
+            }
+
+            customPopUp(
+              icon: Icons.check_circle_outline_rounded,
+              message: 'Berhasil update profile',
+              onTap: () {
+                Get.back();
+                Get.back();
+              },
+              titleButton: 'Kembali',
+            );
+          } else {
+            customPopUp(
+              icon: Icons.cancel_outlined,
+              message: 'Gagal update profile',
+              isSuccess: false,
+              onTap: () {
+                Get.back();
+                Get.back();
+              },
+              titleButton: 'Kembali',
+            );
+          }
+        }
+      }
+    } catch (e) {
+      developer.log(e.toString());
+    }
   }
 
   static Future<QuerySnapshot<Map<String, dynamic>>> getUserByEmail(
