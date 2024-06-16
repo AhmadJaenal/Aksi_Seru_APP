0a3f492f45409537914bef8498fbde6c6fc7d7fe
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-16 11:06:59 +0700

    [+] Implement like&unlike post

--- lib/getX/post.dart
+++ lib/getX/post.dart
@@ -1,42 +1,18 @@
-import 'dart:async';
+import 'package:aksi_seru_app/controller/post_controller.dart';
 
-import '../models/article_model.dart';
 import 'package:get/get.dart';
-import 'dart:developer' as developer;
 
 class LikeUnlikePost extends GetxController {
   RxBool likeState = false.obs;
 
   RxBool get isLiked => likeState;
 
-  void setLikeUnlike({int? id}) {
-    if (likeState.value) {
-      developer.log('unlike');
-    } else {
-      developer.log('like');
-    }
+  void setLikeUnlike({String? idPost}) {
     likeState.value = !likeState.value;
+    if (likeState.value) {
+      PostController.likePost(idPost: idPost);
+    } else {
+      PostController.unlikePost(idPost: idPost);
+    }
   }
 }
-
-// class CommentState extends GetxController {
-//   var comment = <CommentModel>[].obs;
-
-//   int get length => comment.length;
-
-//   void setComment(List<CommentModel> userComment) {
-//     comment.assignAll(userComment);
-//   }
-
-//   void addComment(CommentModel newComment) {
-//     comment.add(newComment);
-//   }
-
-//   void startAutoDeleteTimer() {
-//     Timer.periodic(const Duration(seconds: 10), (timer) {
-//       if (comment.isNotEmpty) {
-//         comment.removeAt(0);
-//       }
-//     });
-//   }
-// }
