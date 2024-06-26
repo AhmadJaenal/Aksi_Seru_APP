b07add54f3941b9a3b65121a356f4e456058255b
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-06-26 22:32:38 +0700

    [+] Implement obx

--- lib/screens/home/article/for_you_page.dart
+++ lib/screens/home/article/for_you_page.dart
@@ -1,3 +1,5 @@
+import 'package:aksi_seru_app/getX/search_state.dart';
+import 'package:get/get.dart';
 import '../../../controller/article_controller.dart';
 import '../../../models/article_model.dart';
 import '../../../shared/style.dart';
@@ -20,35 +22,45 @@ class ForYouPage extends StatelessWidget {
             child: SizedBox(
               width: double.infinity,
               height: MediaQuery.of(context).size.height * .5,
-              child: FutureBuilder<List<dynamic>?>(
-                future: ArticleController.getRecommendArticle(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (context, index) {
-                          ArticleModel dataArticle = snapshot.data![index];
-
-                          return CardArticle(article: dataArticle);
-                        },
-                      );
-                    } else {
-                      return Center(
-                        child: Text(
-                          'Belum ada article yang anda buat',
-                          style: AppTextStyle.h2.copyWith(
-                            color: AppColors.blackColor,
+              child: Obx(() {
+                if (SearcArticlehState.listArticle.isEmpty) {
+                  return FutureBuilder<List<ArticleModel>>(
+                    future: ArticleController.getRecommendArticle(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasData) {
+                        return ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            ArticleModel dataArticle = snapshot.data![index];
+                            return CardArticle(article: dataArticle);
+                          },
+                        );
+                      } else {
+                        return Center(
+                          child: Text(
+                            'Belum ada article yang anda buat',
+                            style: AppTextStyle.h2.copyWith(
+                              color: AppColors.blackColor,
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  }
-                },
-              ),
+                        );
+                      }
+                    },
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: SearcArticlehState.listArticle.length,
+                    itemBuilder: (context, index) {
+                      ArticleModel dataArticle =
+                          SearcArticlehState.listArticle[index];
+                      return CardArticle(article: dataArticle);
+                    },
+                  );
+                }
+              }),
             ),
           ),
         ],
